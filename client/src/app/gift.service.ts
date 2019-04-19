5ecf60c7da9962d0544353f60a0f944493b71f45
Jesse Lind <jesse.lind@gmail.com>
2019-04-19 16:00:22 -0600

    added getGifts() method to gift.service.ts

--- client/src/app/gift.service.ts
+++ client/src/app/gift.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { User } from './user';
 import { Gift } from './gift';
@@ -10,4 +11,13 @@ import { Gift } from './gift';
 export class GiftService {
 
   constructor(private http: HttpClient) { }
+
+  options = { withCredentials: true };
+
+  // base url of the express back end
+  url: string = "http://localhost:3000/users/";
+
+  getGifts(): Observable<Gift[]> {
+    return this.http.get<Gift[]>(this.url + "gifts/:id", this.options);
+  }
 }
